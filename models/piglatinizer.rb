0135b1bda55362058c034fb38a628b16fb497def
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-11 16:34:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,4 +8,11 @@ class PigLatinizer
   def consonant?(char)
     !char.match(/[aeiouAEIOU]/)    
   end
+
+  def piglatinize_word(word)
+    
+  end
+  def piglatinize_sentence(sentence)
+    
+  end
 end
